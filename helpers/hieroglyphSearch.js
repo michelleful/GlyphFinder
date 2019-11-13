d73fd547ffe2e054e749b8de9b5121686117dd95
Michelle Fullwood <michelle.fullwood@gmail.com>
2019-11-13 11:37:08 -0500

    Changed pronunciations to require a tilde in front for better searching; added category H annotations (parts of birds)

--- helpers/hieroglyphSearch.js
+++ helpers/hieroglyphSearch.js
@@ -4,21 +4,19 @@ import * as JsSearch from "js-search";
 var search = new JsSearch.Search("id");
 search.indexStrategy = new JsSearch.PrefixIndexStrategy();
 
-var REGEX = /[^a-zа-яё0-9\-:']+/i;
+var REGEX = /[^a-zа-яё0-9\-:~']+/i;
 search.tokenizer = {
-  tokenize( text ) {
-    return text
-      .split(REGEX)
-      .filter(
-        (text) => text // Filter empty tokens
-      );
+  tokenize(text) {
+    return text.split(REGEX).filter(
+      text => text // Filter empty tokens
+    );
   }
 };
 
 search.addIndex("description");
 search.addIndex("pronunciations");
 search.addIndex("keywords");
-search.addIndex("shape");
+search.addIndex("meta");
 
 search.addDocuments(hieroglyphs);
 
